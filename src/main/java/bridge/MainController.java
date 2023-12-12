54390d84e53545731b43a4302ab17b982b6a693c
jhon3242 <rldotnr2@gmail.com>
2023-12-12 10:24:12 +0900

    feat : 구조 초안 작성

--- src/main/java/bridge/MainController.java
+++ src/main/java/bridge/MainController.java
@@ -1,7 +1,42 @@
 package bridge;
 
+import bridge.domain.Bridge;
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.ArrayList;
+
 public class MainController {
     public static void run() {
+        Bridge answer = initBridge();
+        Bridge user = new Bridge(new ArrayList<>());
+        BridgeGame bridgeGame = new BridgeGame(answer, user);
 
+        proceedBridge(bridgeGame);
+        OutputView.printResult(bridgeGame);
+    }
+
+    private static Bridge initBridge() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int bridgeSize = InputView.readBridgeSize();
+        return new Bridge(bridgeMaker.makeBridge(bridgeSize));
+    }
+
+    private static void proceedBridge(BridgeGame bridgeGame) {
+        while (true) {
+            bridgeGame.move(InputView.readMoving());
+            BridgeResult bridgeResult = bridgeGame.calculate();
+            OutputView.printMap(bridgeResult);
+
+            if (isEnd()) {
+                if (isRetry()) {
+                    bridgeGame.retry();
+                    continue;
+                }
+                break;
+            }
+        }
     }
 }
--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,9 +1,20 @@
 package bridge.domain;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private Bridge answer;
+    private Bridge user;
+    private int trial;
+
+    public BridgeGame(Bridge answer, Bridge user) {
+        this.answer = answer;
+        this.user = user;
+        this.trial = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
